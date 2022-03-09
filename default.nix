a89c480075e4d773cb6146157e16d7405f1454b7
Jon Brede Skaug <rskaze@gmail.com>
2022-03-09 20:25:45 +0100

    Patch neo4j version

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
 let
   sources = import ./nix/sources.nix;
 
-  hello = pkgs.writeShellScriptBin "hello" ''
-    echo "Hello from the Nix channel overlay!"
-  '';
-
   pkgs = import sources.nixpkgs {
     overlays = [
       (self: super: {
-        inherit hello;
+        neo4j = super.neo4j.overrideAttrs (old: {
+          version = "4.4.4";
+          src = super.fetchurl {
+            url = "https://neo4j.com/artifact.php?name=neo4j-community-${version}-unix.tar.gz";
+            sha256 = "46ea100f162c86284c352fa56606565d6d87ffe5fc9369945914d08728eff2be";
+          };
+        });
       })
     ];
   };
