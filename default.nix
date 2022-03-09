7bb09d913502b34197d516abe281167a34d1781f
Jon Brede Skaug <rskaze@gmail.com>
2022-03-09 20:27:24 +0100

    hardcode version

--- default.nix
+++ default.nix
@@ -7,7 +7,7 @@ let
         neo4j = super.neo4j.overrideAttrs (old: {
           version = "4.4.4";
           src = super.fetchurl {
-            url = "https://neo4j.com/artifact.php?name=neo4j-community-${version}-unix.tar.gz";
+            url = "https://neo4j.com/artifact.php?name=neo4j-community-4.4.4-unix.tar.gz";
             sha256 = "46ea100f162c86284c352fa56606565d6d87ffe5fc9369945914d08728eff2be";
           };
         });
