2c2c4949474395c1888743082a11ee4e2d287200
Jon Brede Skaug <rskaze@gmail.com>
2022-03-09 22:03:19 +0100

    remove neo4j overlat

--- default.nix
+++ default.nix
@@ -1,17 +1,6 @@
 let
+  # Import sources
   sources = import ./nix/sources.nix;
 
-  pkgs = import sources.nixpkgs {
-    overlays = [
-      (self: super: {
-        neo4j = super.neo4j.overrideAttrs (old: {
-          version = "3.5.14";
-          src = super.fetchurl {
-            url = "https://neo4j.com/artifact.php?name=neo4j-community-3.5.14-unix.tar.gz";
-            sha256 = "1zjb6cgk2lpzx6pq1cs5fh65in6b5ccpl1cgfiglgpjc948mnhzv";
-          };
-        });
-      })
-    ];
-  };
-in pkgs
+# And return that specific nixpkgs
+in sources.nixpkgs
